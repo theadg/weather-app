eecad79f34fe4e3f959558e69e24aeb6a3bffe95
theadg <deguzmanbernardandrew@gmail.com>
2022-12-10 00:04:26 +0800

    Add weather icon

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import './styles/style.scss';
+
 const weatherLocation = document.querySelector('#weatherLocation');
 const getWeatherButton = document.querySelector('#getWeather');
-
+const weatherIcon = document.querySelector('#weatherIcon');
 const getWeather = async (location, unit = 'metric') => {
   if (location === '') {
     // throw error or alert here
@@ -13,6 +15,7 @@ const getWeather = async (location, unit = 'metric') => {
 
   //   this transforms the promise into usable data
   const weatherData = await response.json();
+  weatherIcon.src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
   console.log(weatherData);
 };
 
@@ -41,3 +44,9 @@ window.onload = () => getForecast(weatherLocation.value);
 
 // get to display the data
 // https://api.openweathermap.org/data/2.5/weather?q=London&APPID=5cefd4fab8a0f1d0f39eb7f546ef57ea
+
+// trying to use the icon
+
+// const getWeatherIcon = async (iconNumber) => {
+//     `http://openweathermap.org/img/wn/${iconNumber}@2x.png`
+// };
